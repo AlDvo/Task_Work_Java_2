b69bb13dacb4e5cc75dfbf41712bc85ad38cba7f
AlDvo <Dvorenenko@inbox.ru>
2021-04-24 12:48:11 +0300

    Задание 7

---
+++ src/main/java/TaskWork7.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TaskWork7 {
+
+    static class Calc {
+        double one;
+        double two;
+        String operation;
+
+        Calc(double one , double two , String operation){
+            this.one = one;
+            this.two = two;
+            this.operation = operation;
+        }
+
+        double sum(){
+            return one + two;
+        }
+        double minus(){
+            return one - two;
+        }
+        double proizv(){
+            return one * two;
+        }
+        double delenie(){
+            return one / two;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // считываем числа с клавиатуры
+        System.out.print("Введите первое число ");
+        double one = 0;
+        try {
+            one = Double.parseDouble(reader.readLine());
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("Не введено первое число");
+        }
+
+        System.out.print("Введите второе число ");
+        double two = 0;
+        try {
+            two = Double.parseDouble(reader.readLine());
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("Не введено второе число");
+        }
+        // Запрашивем название операции
+        System.out.print("Какую операцию вы хотите выполнить:сложение, вычитание, деление, умножение  ");
+        String operation =  reader.readLine();
+
+        Calc calc = new Calc(one , two , operation);
+
+        switch (operation){
+            case ("сложение"):
+                System.out.printf("Сумма чисел  = %.4f", calc.sum());
+                break;
+            case ("вычитание"):System.out.printf("Вычитание чисел  = %.4f", calc.minus());
+                break;
+            case ("деление"):
+                System.out.printf("Деление чисел  = %.4f", calc.delenie());
+                break;
+            case ("умножение"):System.out.printf("Произведение чисел  = %.4f", calc.proizv());
+                break;
+            default:System.out.print("Не правильно введено название операции");
+               break;
+        }
+    }
+}
+
